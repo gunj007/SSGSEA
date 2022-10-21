fdaa32d8be83522687c277a2027977c4aba39b7a
GUNJAN Arya <110597928+gunj007@users.noreply.github.com>
2022-10-22 01:57:38 +0530

    Update ssgea.py

--- ssgea.py
+++ ssgea.py
@@ -11,29 +11,4 @@ print(data,data.columns)
 
 data.drop(['description','ensembl_gene_id'],axis=1,inplace=True)
 data.to_csv("log_values.csv",index=False)
-'''
-#convertinng to numpy array (matrix)
-try:
-    cpm=[]
-    for i in range(1,len(data)):
-            colsum=data.iloc[:,i].sum()
-
-            c1=(data.iloc[:,i]/colsum)*10**6
-            cpm.append(c1)
-    
-except:
-    print("ignore error")
-print(cpm)
-cpm=np.array(cpm)
-print(cpm)
-logv=np.log2(cpm +1)
-print("logv",logv)
-
-log_value=pd.DataFrame(logv).T
-log_value.columns=data.columns[1:13]
-log_value['gene_id']=gene_id
-
-col_1=log_value.pop('gene_id')
-log_value.insert(0,'gene_id',col_1)
-print("logval:",log_value)
-#log_value.to_csv("log_value.csv",index=False)'''
+#further code in ssgea.r file
